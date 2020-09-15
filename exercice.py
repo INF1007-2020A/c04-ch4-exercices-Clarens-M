c3250010d4441ce3957f8991aef7cdc98fd937e4
Clarens-M <clarens.mpouo@polymtl.ca>
2020-09-15 22:45:05 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -11,7 +11,7 @@ def is_even_len(string: str) -> bool:
 
 def remove_third_char(string: str) -> str:
     if len(string) >= 3:
-        string
+        for c in string
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
