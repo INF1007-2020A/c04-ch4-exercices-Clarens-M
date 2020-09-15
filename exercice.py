f7e75d12b64fe47b2c297dcfb9d7d96dc951e03a
Clarens-M <clarens.mpouo@polymtl.ca>
2020-09-15 23:28:29 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -48,7 +48,7 @@ def get_number_of_words(sentence: str, word: str) -> int:
     for c in sentence:
         if c != " ":
             mot += c
-        elif:
+        elif mot == word:
             
     pass
 
