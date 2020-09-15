6dec90c7d03962754469952a003cc54358ea2a58
Clarens-M <clarens.mpouo@polymtl.ca>
2020-09-15 22:49:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -19,7 +19,7 @@ def remove_third_char(string: str) -> str:
                 i += 1
             else:
                 i += 1
-    string_
+    string_sans_3 = str(liste)
     return string_sans_3
     pass
 
